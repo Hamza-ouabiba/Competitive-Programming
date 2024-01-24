51666509e4d94d5b87a6e056723725ab8e6fd7a2
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2024-01-24 11:49:56 +0100

    Create diaa_does_smssv2.cpp

---
+++ diaa_does_smssv2.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+	int m;
+	int matches = 0;
+    cin >> n;
+
+    string str1, str2, str3;
+    map<string, string> msis;
+	map<string, string> regions;
+    if (n >= 0 && n <= 100) {
+        while(n > 0)
+		{
+			cin >> str1;
+			msis[str1.substr(str1.find('+'),str1.find(','))] = str1.substr(str1.find(',')+1); 
+			n--;
+		}
+
+		cin>>m;
+
+		if(m>= 1 && m<= 100)
+		{
+			str1.clear();
+			while(m > 0)
+			{
+				cin>>str1;
+				regions[str1.substr(str1.find('+'))] = str1.substr(0,str1.find(','));
+				m--;
+			}
+		}
+		
+		for (const auto &pair : regions) {
+			if(msis[pair.first] != "")
+			{
+				cout << pair.first <<","<<msis[pair.first]<<","<<pair.second<<endl;
+				matches = 1;
+			}
+		}
+		if(matches == 0)
+		{
+			cout<<"nomatch";
+		}
+    }
+	
+
+    return 0;
+}
